2eee03235a7c8d4d69963bb846cb5c596a75a754
SebastiaoKomada <sebastiao.komada15@hotmail.com>
2025-08-09 19:44:23 -0300

    adicionado flyway para migration

---
+++ target/classes/db/migration/V2__Populate_table_person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Santa Rita do Sapucai - MG', 'Sebastiao', 'Male', 'Komada'),
+	(2, 'Lorena - SP', 'Ana', 'Female', 'Silva'),
+    (3, 'Guaratingueta - SP', 'Ana', 'Female', 'Komada'),
+    (4, 'Guaratingueta - SP', 'Mario', 'Male', 'Silva'),
+    (5, 'Guaratingueta - SP', 'Ricardo', 'Male', 'Honorio');
